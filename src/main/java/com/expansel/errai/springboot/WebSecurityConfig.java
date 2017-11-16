7a485d057da76f94160cc55770c787275aec2679
BenDol <dolb90@gmail.com>
2017-11-16 17:08:15 +1300

    Update for 0.3.1 release (supporting CSRF)

--- src/main/java/com/expansel/errai/springboot/WebSecurityConfig.java
+++ src/main/java/com/expansel/errai/springboot/WebSecurityConfig.java
@@ -6,17 +6,23 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.expansel.errai.springsecurity.server.ErraiWebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends ErraiWebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // disabling CSRF as it messes with Errai MessageBus, still need to
-        // figure out how to fix it
-        http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().antMatchers("/**").authenticated()
-                .and().formLogin().defaultSuccessUrl("/", true).and().httpBasic();
+        http.authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/**").authenticated()
+        .and()
+            .formLogin()
+                .defaultSuccessUrl("/", true)
+        .and()
+            .httpBasic();
     }
 
     @Autowired
